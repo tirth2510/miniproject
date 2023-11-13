db64a7f8169394b56ee528242eb6ae064ddf92d5
Tirth <trth25102003@gmail.com>
2023-11-13 15:42:40 +0530

    mini

---
+++ D8/project/components/footer.php
@@ -0,0 +1,33 @@
+<!-- footer section starts  -->
+
+<footer class="footer">
+
+   <section class="flex">
+
+      <div class="box">
+         <a href="tel:1234567890"><i class="fas fa-phone"></i><span>9969978163</span></a>
+         <a href="tel:1112223333"><i class="fas fa-phone"></i><span>9082198360</span></a>
+         <a href="mailto:shaikhanas@gmail.com"><i class="fas fa-envelope"></i><span>xyz@gmail.com</span></a>
+         <a href="#"><i class="fas fa-map-marker-alt"></i><span>KJSCE Mumbai 400081</span></a>
+      </div>
+
+      <div class="box">
+         <a href="home.php"><span>Home</span></a>
+         <a href="about.php"><span>About</span></a>
+         <a href="contact.php"><span>Contact</span></a>
+      </div>
+
+      <div class="box">
+         <a href="#"><span>Facebook</span><i class="fab fa-facebook-f"></i></a>
+         <a href="#"><span>Twitter</span><i class="fab fa-twitter"></i></a>
+         <a href="#"><span>Linkedin</span><i class="fab fa-linkedin"></i></a>
+         <a href="#"><span>Instagram</span><i class="fab fa-instagram"></i></a>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>Tirth, Krishiv</span> | all rights reserved!</div>
+
+</footer>
+
+<!-- footer section ends -->
